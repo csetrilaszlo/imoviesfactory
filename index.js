67e28410bb7b83b0342b64b1f70648ea56bb3438
csetrilaszlo <79114152+csetrilaszlo@users.noreply.github.com>
2021-06-24 20:38:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,101 @@
+function changeImageFirst() {
+    console.log('Working');
+    let x = document.getElementById("itemOne");
+    let plus = document.getElementById('plus');
+    let cancel = document.getElementById('cancel');
+   
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      plus.style.display = 'none';
+      cancel.style.display = 'block';
+    } else {
+      x.style.display = "none";
+      plus.style.display = 'block';
+      cancel.style.display = 'none';
+    }
+}  
+
+function changeImageSecond() {
+    console.log('Working');
+    let x = document.getElementById("itemTwo");
+    let plus2 = document.getElementById('plus2');
+    let cancel2 = document.getElementById('cancel2');
+   
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      plus2.style.display = 'none';
+      cancel2.style.display = 'block';
+    } else {
+      x.style.display = "none";
+      plus2.style.display = 'block';
+      cancel2.style.display = 'none';
+    }
+}  
+
+function changeImageThird() {
+    console.log('Working');
+    let x = document.getElementById("itemThree");
+    let plus3 = document.getElementById('plus3');
+    let cancel3 = document.getElementById('cancel3');
+   
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      plus3.style.display = 'none';
+      cancel3.style.display = 'block';
+    } else {
+      x.style.display = "none";
+      plus3.style.display = 'block';
+      cancel3.style.display = 'none';
+    }
+}  
+
+function changeImageFourth() {
+    console.log('Working');
+    let x = document.getElementById("itemFour");
+    let plus4 = document.getElementById('plus4');
+    let cancel4 = document.getElementById('cancel4');
+   
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      plus4.style.display = 'none';
+      cancel4.style.display = 'block';
+    } else {
+      x.style.display = "none";
+      plus4.style.display = 'block';
+      cancel4.style.display = 'none';
+    }
+}
+
+function changeImageFifth() {
+    console.log('Working');
+    let x = document.getElementById("itemFive");
+    let plus5 = document.getElementById('plus5');
+    let cancel5 = document.getElementById('cancel5');
+   
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      plus5.style.display = 'none';
+      cancel5.style.display = 'block';
+    } else {
+      x.style.display = "none";
+      plus5.style.display = 'block';
+      cancel5.style.display = 'none';
+    }
+}
+
+function changeImageSixth() {
+    console.log('Working');
+    let x = document.getElementById("itemSix");
+    let plus6 = document.getElementById('plus6');
+    let cancel6 = document.getElementById('cancel6');
+   
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      plus6.style.display = 'none';
+      cancel6.style.display = 'block';
+    } else {
+      x.style.display = "none";
+      plus6.style.display = 'block';
+      cancel6.style.display = 'none';
+    }
+}
